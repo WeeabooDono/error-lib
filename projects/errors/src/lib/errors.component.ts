ff78b893b6134eb9369eaf052e75419cdf1bbeae
Corentin Beuchet (J64483) <corentin.beuchet@edf.fr>
2023-10-11 11:16:07 +0200

    librairie ajoutée

---
+++ projects/errors/src/lib/errors.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-errors',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      errors works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ErrorsComponent {
+
+}
---
+++ projects/errors/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of errors
+ */
+
+export * from './lib/errors.service';
+export * from './lib/errors.component';
